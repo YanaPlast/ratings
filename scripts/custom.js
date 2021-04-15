9de19eee958bb8ba0b824d9563c9a0e7817ad674
Yana <yana.vsu@gmail.com>
2021-04-15 17:47:59 +0300

    new page - otzyvy

--- scripts/custom.js
+++ scripts/custom.js
@@ -38,6 +38,123 @@ $(document).ready(function() {
         }
         
     })
+// Инициализация плагина simplePagination
 
+// $('#light-pagination').pagination({
+//     items: 33,
+//     itemsOnPage: 1,
+//     cssStyle: 'light-theme'
+// });
+
+// slider 
+
+// var slideNow = 1;
+// var slideCount = $('.slider__wrapper').children().length;
+// var slideTime = 1000;
+
+// //setInterval(nextSlide, slideTime);
+
+// $('.slider__btn_next').click(nextSlide);
+// $('.slider__btn_prev').click(prevSlide);
+
+// function nextSlide(){
+//     if(slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+//         $('.slider__wrapper').css({
+//             'transform': 'translate(0,0)',
+//             '-o-transform': 'translate(0,0)',
+//             '-webkit-transform': 'translate(0,0)'
+//         });
+//         slideNow = 1;
+//     } else {
+//         var translateWidth = -$('.slider__viewport').width()*(slideNow);
+//         $('.slider__wrapper').css({
+//             'transform': 'translate('+translateWidth+'px,0)',
+//             '-o-transform': 'translate('+translateWidth+'px,0)',
+//             '-webkit-transform': 'translate('+translateWidth+'px,0)'
+//         });
+//         slideNow ++;
+//     }
+// }
+
+// function prevSlide(){
+//     if(slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+//         var translateWidth = -$('.slider__viewport').width()*(slideCount - 1);
+//         $('.slider__wrapper').css({
+//             'transform': 'translate('+translateWidth+'px,0)',
+//             '-o-transform': 'translate('+translateWidth+'px,0)',
+//             '-webkit-transform': 'translate('+translateWidth+'px,0)'
+//         });
+//         slideNow = slideCount;
+//     } else {
+//         var translateWidth = -$('.slider__viewport').width()*(slideNow - 2);
+//         $('.slider__wrapper').css({
+//             'transform': 'translate('+translateWidth+'px,0)',
+//             '-o-transform': 'translate('+translateWidth+'px,0)',
+//             '-webkit-transform': 'translate('+translateWidth+'px,0)'
+//         });
+//         slideNow --;
+//     }
+// }
+
+
+var slideNow = 1;
+var slideCount = $('.slider__wrapper').children().length;
+var slideTime = 1000;
+
+//setInterval(nextSlide, slideTime);
+
+$('.slider__btn_next').click(nextSlide);
+$('.slider__btn_prev').click(prevSlide);
+
+function nextSlide(){
+    if(slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+        $('.slider__wrapper').css({
+            'transform': 'translate(0,0)',
+            '-o-transform': 'translate(0,0)',
+            '-webkit-transform': 'translate(0,0)'
+        });
+        slideNow = 1;
+    } else {
+        var translateWidth = (-$('.pagination__filter').width()*(slideNow) - 5);
+        $('.slider__wrapper').css({
+            'transform': 'translate('+translateWidth+'px,0)',
+            '-o-transform': 'translate('+translateWidth+'px,0)',
+            '-webkit-transform': 'translate('+translateWidth+'px,0)'
+        });
+        slideNow ++;
+    }
+}
+
+function prevSlide(){
+    if(slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+        var translateWidth = (-$('.pagination__filter').width()*(slideCount - 1) - 5);
+        $('.slider__wrapper').css({
+            'transform': 'translate('+translateWidth+'px,0)',
+            '-o-transform': 'translate('+translateWidth+'px,0)',
+            '-webkit-transform': 'translate('+translateWidth+'px,0)'
+        });
+        slideNow = slideCount;
+    } else {
+        var translateWidth = (-$('.pagination__filter').width()*(slideNow - 2) - 5);
+        $('.slider__wrapper').css({
+            'transform': 'translate('+translateWidth+'px,0)',
+            '-o-transform': 'translate('+translateWidth+'px,0)',
+            '-webkit-transform': 'translate('+translateWidth+'px,0)'
+        });
+        slideNow --;
+    }
+}
+
+
+    // Кнопка "Подробнее"
+    $('.button-more span').on('click', function () {
+        console.log('Hello');
+        $(this).closest('.feedback__text').find('.more-text').toggleClass('more-text_hidden');
+        if ($(this).closest('.feedback__text').find('.more-text').hasClass('more-text_hidden')) {
+            $(this).text('Подробнее...');
+        } else {
+            $(this).text('Свернуть');
+        }
+    });
 
 });
