1b3d8c45e9edac293793262b32f20127af866f73
Yana <yaplas.zhe@gmail.com>
2021-04-20 16:55:24 +0300

    colours, bg-s, alphab slider...

--- scripts/custom.js
+++ scripts/custom.js
@@ -38,66 +38,9 @@ $(document).ready(function() {
         }
         
     })
-// Инициализация плагина simplePagination
-
-// $('#light-pagination').pagination({
-//     items: 33,
-//     itemsOnPage: 1,
-//     cssStyle: 'light-theme'
-// });
-
-// slider 
-
-// var slideNow = 1;
-// var slideCount = $('.slider__wrapper').children().length;
-// var slideTime = 1000;
-
-// //setInterval(nextSlide, slideTime);
-
-// $('.slider__btn_next').click(nextSlide);
-// $('.slider__btn_prev').click(prevSlide);
-
-// function nextSlide(){
-//     if(slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
-//         $('.slider__wrapper').css({
-//             'transform': 'translate(0,0)',
-//             '-o-transform': 'translate(0,0)',
-//             '-webkit-transform': 'translate(0,0)'
-//         });
-//         slideNow = 1;
-//     } else {
-//         var translateWidth = -$('.slider__viewport').width()*(slideNow);
-//         $('.slider__wrapper').css({
-//             'transform': 'translate('+translateWidth+'px,0)',
-//             '-o-transform': 'translate('+translateWidth+'px,0)',
-//             '-webkit-transform': 'translate('+translateWidth+'px,0)'
-//         });
-//         slideNow ++;
-//     }
-// }
-
-// function prevSlide(){
-//     if(slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
-//         var translateWidth = -$('.slider__viewport').width()*(slideCount - 1);
-//         $('.slider__wrapper').css({
-//             'transform': 'translate('+translateWidth+'px,0)',
-//             '-o-transform': 'translate('+translateWidth+'px,0)',
-//             '-webkit-transform': 'translate('+translateWidth+'px,0)'
-//         });
-//         slideNow = slideCount;
-//     } else {
-//         var translateWidth = -$('.slider__viewport').width()*(slideNow - 2);
-//         $('.slider__wrapper').css({
-//             'transform': 'translate('+translateWidth+'px,0)',
-//             '-o-transform': 'translate('+translateWidth+'px,0)',
-//             '-webkit-transform': 'translate('+translateWidth+'px,0)'
-//         });
-//         slideNow --;
-//     }
-// }
 
 
-var slideNow = 1;
+/* var slideNow = 1;
 var slideCount = $('.slider__wrapper').children().length;
 var slideTime = 1000;
 
@@ -143,10 +86,83 @@ function prevSlide(){
         });
         slideNow --;
     }
+} */
+
+
+
+var slideNow = 1;
+var slideCount = $('.slider__wrapper').children().length;
+var slideTime = 1000;
+
+//setInterval(nextSlide, slideTime);
+
+$('.slider__btn_next').click(nextSlide);
+$('.slider__btn_prev').click(prevSlide);
+$('.slider__btn_start').click(startSlide);
+$('.slider__btn_end').click(endSlide);
+
+
+function startSlide () {
+    $('.slider__wrapper').css({
+        'transform': 'translate(0,0)',
+        '-o-transform': 'translate(0,0)',
+        '-webkit-transform': 'translate(0,0)'
+    });
+    slideNow = 1;
 }
 
+ function endSlide () {
+     $('.slider__wrapper').css({
+        'transform': 'translate(-660px,0)',
+        '-o-transform': 'translate(-660px,0)',
+        '-webkit-transform': 'translate(-660px,0)'
+    });
+    slideNow = slideCount-8;
+ }
 
-    // Кнопка "Подробнее"
+function nextSlide(){
+    if(slideNow == 21 || slideNow <= 0 || slideNow > 21) {
+        $('.slider__wrapper').css({
+            'transform': 'translate(0,0)',
+            '-o-transform': 'translate(0,0)',
+            '-webkit-transform': 'translate(0,0)'
+        });
+        slideNow = 1;
+    } else {
+        var translateWidth = (-33*(slideNow) );
+        $('.slider__wrapper').css({
+            'transform': 'translate('+translateWidth+'px,0)',
+            '-o-transform': 'translate('+translateWidth+'px,0)',
+            '-webkit-transform': 'translate('+translateWidth+'px,0)'
+        });
+        slideNow ++;
+    }
+}
+
+function prevSlide(){
+    if(slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+        var translateWidth = (-33*(20));
+        $('.slider__wrapper').css({
+            'transform': 'translate('+translateWidth+'px,0)',
+            '-o-transform': 'translate('+translateWidth+'px,0)',
+            '-webkit-transform': 'translate('+translateWidth+'px,0)'
+        });
+        slideNow = slideCount-8;
+    } else {
+        var translateWidth = (-33*(slideNow - 2));
+        $('.slider__wrapper').css({
+            'transform': 'translate('+translateWidth+'px,0)',
+            '-o-transform': 'translate('+translateWidth+'px,0)',
+            '-webkit-transform': 'translate('+translateWidth+'px,0)'
+        });
+        slideNow --;
+    }
+} 
+
+
+
+
+// Кнопка "Подробнее"
     $('.button-more span').on('click', function () {
         $(this).closest('.feedback__text').find('.more-text').toggleClass('more-text_hidden');
         if ($(this).closest('.feedback__text').find('.more-text').hasClass('more-text_hidden')) {
